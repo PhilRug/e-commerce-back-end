8468b1fc27a52029c1f0e85a4186aadc05455d3c
PhilRug <ruggiero.pj@gmail.com>
2023-04-23 17:44:04 -0400

    fixed body

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,7 +32,7 @@ router.get('/:id', async (req, res) => {
 // create a new category
 router.post('/', async (req, res) => {
   try {
-    const newCategory = await Category.create(req, body);
+    const newCategory = await Category.create(req.body);
     res.status(200).json(newCategory);
   } catch (err) {
     res.status(500).json(err);
